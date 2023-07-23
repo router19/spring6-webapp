a03cb50d923392cb1e09e46d762d77d00fd82e19
Vinit Kumar <vinitadroit@gmail.com>
2023-07-23 19:28:46 +0530

    Initial commit

---
+++ src/test/java/com/vinikuma/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinikuma.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
