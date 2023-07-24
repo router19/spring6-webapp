049188d10d1870e53f3bac5976ce02962a528ee0
Vinit Kumar <vinitadroit@gmail.com>
2023-07-24 10:43:30 +0530

    added entity , the persistence layer

---
+++ src/main/java/com/vinikuma/spring6webapp/domain/Author.java
@@ -0,0 +1,20 @@
+package com.vinikuma.spring6webapp.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+
+@Data
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+}
---
+++ src/main/java/com/vinikuma/spring6webapp/domain/Book.java
@@ -0,0 +1,22 @@
+package com.vinikuma.spring6webapp.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+}
